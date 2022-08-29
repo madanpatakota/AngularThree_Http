f8fcf83d051616ef5eb3185f0be88c93fda483e1
MadanMohanReddy <madan.patakota@hotmail.com>
2022-08-29 08:58:41 +0530

    Get , Post , Put , Delete , Patch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularThree_Http';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ProductsData: any = [];
+  ngOnInit(): void {
+    // this.httpClient.get("https://jsonplaceholder.typicode.com/todos/")
+    // .subscribe((result)=>{
+    //   console.log("Result from Jsonplaceholder api" ,result);
+    //   this.mydata = result;
+    // })
+    this.httpClient
+      .get('http://localhost:3000/Products')
+      .subscribe((result) => {
+        console.log(result);
+        this.ProductsData = result;
+      });
+  }
+
+  PostProduct() {
+    this.httpClient
+      .post('http://localhost:3000/Products', {
+        id: 7,
+        Name: 'Lenovo_First_version',
+        Price: '10000',
+        Location: 'Bangolore',
+      })
+      .subscribe((result) => {
+        //console.log(result);
+        this.ProductsData.push(result);
+      });
+  }
+
+
+  // {
+  //   "id": 3,
+  //   "Name": "HP",
+  //   "Price": "20000",
+  //   "Location": "Chennai"
+  // },
+
+  UpdateProduct() {
+    this.httpClient
+      .put('http://localhost:3000/Products/1', {
+          "Name": "xyz",
+          "Price": "10000",
+          "Location": "Delhi"
+      })
+      .subscribe((result:any) => {
+        console.log(result);
+        this.ProductsData.push(result);
+        // let data = this.ProductsData as [];
+        // let record = data.filter((x:any,index)=> x.id == result.id)[0];
+      });
+  }
+
+
+  PatchProduct() {
+    this.httpClient
+      .patch('http://localhost:3000/Products/1', {
+          "Name": "AcerTwo",
+      })
+      .subscribe((result:any) => {
+        console.log(result);
+        //this.ProductsData.push(result);
+        // let data = this.ProductsData as [];
+        // let record = data.filter((x:any,index)=> x.id == result.id)[0];
+      });
+  }
+
+
+
+  DeleteProduct() {
+    this.httpClient
+      .delete('http://localhost:3000/Products/2')
+      .subscribe((result:any) => {
+        console.log(result);
+        //this.ProductsData.push(result);
+        // let data = this.ProductsData as [];
+        // let record = data.filter((x:any,index)=> x.id == result.id)[0];
+      });
+  }
+
+
+
+
+
+  //for doing the subscription i need the some setup
 }
